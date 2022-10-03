84240a34abba83b82f5ff61e15c0f390d0478392
Ilya Trubachev <trubachev.ilya@huawei.com>
2022-10-04 01:19:21 +0300

    Graph added, no tests

--- basic_block.h
+++ basic_block.h
@@ -8,13 +8,13 @@
 
 #include "inst.h"
 
-template <uint32_t bb_id, uint32_t... successors>
 class BasicBlock {
 public:
-    BasicBlock(std::initializer_list<InstNode*> insts);
+    template <uint32_t bb_id, uint32_t... successors>
+    static BasicBlock *BasicBlockBuilder(std::initializer_list<InstNode*> insts);
+    static void BasicBlockDestroyer(BasicBlock *bb);
 
     void Dump();
-    void Clear();
 
     InstNode *GetFirstinst() const
     {
@@ -31,12 +31,41 @@ public:
         return id;
     }
 
+    const std::vector<uint32_t>& GetPreds()
+    {
+        return preds;
+    }
+
+    const std::vector<uint32_t>& GetSuccs()
+    {
+        return succs;
+    }
+
+    // TODO clarify usage of rvalue refs
+    void SetPreds(std::vector<uint32_t>&& predecessors)
+    {
+        preds = std::move(predecessors);
+    }
+
+    void SetPredsRef(std::vector<BasicBlock*>&& predecessors_ref)
+    {
+        preds_ref = std::move(predecessors_ref);
+    }
+
+    void SetSuccsRef(std::vector<BasicBlock*>&& successors_ref)
+    {
+        succs_ref = std::move(successors_ref);
+    }
+
 private:
     BasicBlock() = default;
     BasicBlock(BasicBlock &bb) = default;
 
+    // These guys are binded in Graph constructor
     std::vector<uint32_t> preds;
+    std::vector<BasicBlock*> preds_ref;
     std::vector<uint32_t> succs;
+    std::vector<BasicBlock*> succs_ref;
 
     InstNode *first_inst = nullptr;
     InstNode *last_inst = nullptr;
@@ -47,20 +76,23 @@ private:
 };
 
 template <uint32_t bb_id, uint32_t... successors>
-BasicBlock<bb_id, successors...>::BasicBlock(std::initializer_list<InstNode*> insts)
+BasicBlock *BasicBlock::BasicBlockBuilder(std::initializer_list<InstNode*> insts)
 {
+    // TODO Change, since empty bbs and bbs with 1 instruction exists!
     if (insts.size() < 2) {
         std::cerr << "invalid number of instructions in basic block" << std::endl;
         std::abort();
     }
+    
+    BasicBlock *result = new BasicBlock;
 
-    first_inst = *(insts.begin());
-    last_inst = *(std::prev(insts.end(), 1));
-    first_inst->SetNext(*(std::next(insts.begin(), 1)));
-    last_inst->SetPrev(*(std::prev(insts.end(), 2)));
-    first_inst->SetBBid(bb_id);
-    last_inst->SetBBid(bb_id);
-
+    // Bind instructions within basic block
+    result->first_inst = *(insts.begin());
+    result->last_inst = *(std::prev(insts.end(), 1));
+    result->first_inst->SetNext(*(std::next(insts.begin(), 1)));
+    result->last_inst->SetPrev(*(std::prev(insts.end(), 2)));
+    result->first_inst->SetBBid(bb_id);
+    result->last_inst->SetBBid(bb_id);
     for (auto item = std::next(insts.begin(), 1); item < std::prev(insts.end(), 1); std::advance(item, 1))
     {
         (*item)->SetBBid(bb_id);
@@ -69,27 +101,41 @@ BasicBlock<bb_id, successors...>::BasicBlock(std::initializer_list<InstNode*> in
         (*item)->SetNext(*(std::next(item, 1)));
     }
 
-    id = bb_id;
-    succs = std::vector{successors...};
-}
-
-template <uint32_t bb_id, uint32_t... successors>
-void BasicBlock<bb_id, successors...>::Dump()
-{
-    for (InstNode *item = first_inst; item != nullptr; item = item->GetNext())
-    {
-        item->Dump();
+    result->id = bb_id;
+    if constexpr (sizeof...(successors) != 0) {
+        result->succs = std::vector{successors...};
     }
+
+    return result;
 }
 
-template <uint32_t bb_id, uint32_t... successors>
-void BasicBlock<bb_id, successors...>::Clear()
+void BasicBlock::BasicBlockDestroyer(BasicBlock *bb)
 {
-    for (InstNode *item = first_inst; item != nullptr;)
+    assert(bb != nullptr);
+    for (InstNode *item = bb->GetFirstinst(); item != nullptr;)
     {   
         InstNode *curr = item;
         item = item->GetNext();
         InstNode::InstDestroyer(curr);
+    }
+
+    delete bb;
+}
+
+void BasicBlock::Dump()
+{
+    std::cout << "bb id " << id << "\npreds [ ";
+    for (auto item: preds) {
+        std::cout << item << " ";
+    }
+    std::cout << "]\nsuccs [ ";
+    for (auto item: succs) {
+        std::cout << item << " ";
+    }
+    std::cout << "]\n";
+    
+    for (InstNode *item = first_inst; item != nullptr; item = item->GetNext()) {
+        item->Dump();
     }
 }
 
